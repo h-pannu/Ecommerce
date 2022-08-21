af462d4506ea0a98d8cd5a55c967a69892fe5c37
hpannu <hpannu@1IS-HARPREET-LT>
2022-08-20 21:04:42 -0700

    Add project files.

---
+++ Server/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly DataContext _Context;
+        public ProductController(DataContext context)
+        {
+            _Context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProduct()
+        {
+            var products = await _Context.Products.ToListAsync();
+            return Ok(products);
+        }
+    }
+}
---
+++ Shared/ReadMe.txt
@@ -0,0 +1,15 @@
+﻿Package Manager Console:   dotnet tool install --global dotnet-ef  
+
+nuget packages:
+   install Microsoft.EntityFrameworkCore 
+   install Microsoft.EntityFrameworkCore.Design
+   install Microsoft.EntityFrameworkCore.SqlServer
+
+PM> cd Server
+PM> dotnet ef migrations add createInitial   //add migration
+
+PM> dotnet ef migrations remove    //delete migration
+
+PM> dotnet ef database update      //Update database from migration
+
+PM> dotnet ef migrations add ProductSeeding  //Seed data
