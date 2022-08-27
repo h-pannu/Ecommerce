e82224d6b4d24d9f3583daacebdb3f5e2e1ade55
harpreet pannu <er.harpreetpannu@live.com>
2022-08-26 17:21:44 -0700

    search component

--- Client/Services/ProductService/ProductService.cs
+++ Client/Services/ProductService/ProductService.cs
@@ -8,6 +8,7 @@
             _http = http;
         }
         public List<Product> Products { get; set; } = new List<Product>();
+        public string Message { get; set; } = "Loading Products...";
 
         public event Action ProductsChanged;
 
@@ -28,5 +29,20 @@
             ProductsChanged.Invoke();
         }
 
+        public async Task<List<string>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchsuggestions/{searchText}");
+            return result.Data;
+        }
+
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>> ($"api/product/search/{searchText}");
+            if(result!=null && result.Data!=null)
+                Products=result.Data;
+            if (Products.Count == 0)
+                Message = "No products found.";
+            ProductsChanged?.Invoke();
+        }
     }
 }
