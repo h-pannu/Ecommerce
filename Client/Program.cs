6e8bf9262bc3cc087055defbc0b2bca3f4e7fd70
harpreet pannu <er.harpreetpannu@live.com>
2022-08-28 14:34:54 -0700

    Cart counter

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,13 +4,17 @@ global using Ecommerce.Client.Services;
 using Ecommerce.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICartService, CartService>();
 
 await builder.Build().RunAsync();
---
+++ Client/Services/CartService/ICartService.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.Client.Services
+{
+    public interface ICartService
+    {
+        event Action OnChange;
+        Task AddToCart(CartItem cartItem);
+        Task<List<CartItem>> GetCartItems();
+    }
+}
